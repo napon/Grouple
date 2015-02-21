336ed952b51237b072e36a7d70ec579f6b4b3295
dan-l <daniel9618@gmail.com>
2015-02-21 13:08:23 -0800

    Setup angular

--- www/js/index.js
+++ www/js/index.js
@@ -16,34 +16,73 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+angular.module('grouple', ['ngRoute', 'login']).
+config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/login'});
+}]);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+// angular.module('grouple', ['login'])
+// .controller('AppController',['login', function(Login) {
+//     // Application Constructor
+//     this.initialize = function(){
+//         this.bindEvents();
+//     };
+//     // Bind Event Listeners
+//     //
+//     // Bind any events that are required on startup. Common events are:
+//     // 'load', 'deviceready', 'offline', and 'online'.
+//     this.bindEvents = function() {
+//         document.addEventListener('deviceready', this.onDeviceReady, false);
+//     };
+//     // deviceready Event Handler
+//     //
+//     // The scope of 'this' is the event. In order to call the 'receivedEvent'
+//     // function, we must explicitly call 'app.receivedEvent(...);'
+//     this.onDeviceReady = function() {
+//         app.receivedEvent('deviceready');
+//     };
+//     // Update DOM on a Received Event
+//     receivedEvent = function(id) {
+//         var parentElement = document.getElementById(id);
+//         var listeningElement = parentElement.querySelector('.listening');
+//         var receivedElement = parentElement.querySelector('.received');
 
-        console.log('Received Event: ' + id);
-    }
-};
+//         listeningElement.setAttribute('style', 'display:none;');
+//         receivedElement.setAttribute('style', 'display:block;');
+
+//         console.log('Received Event: ' + id);
+//     };
+// }]);
+
+
+// var app = {
+//     // Application Constructor
+//     initialize: function() {
+//         this.bindEvents();
+//     },
+//     // Bind Event Listeners
+//     //
+//     // Bind any events that are required on startup. Common events are:
+//     // 'load', 'deviceready', 'offline', and 'online'.
+//     bindEvents: function() {
+//         document.addEventListener('deviceready', this.onDeviceReady, false);
+//     },
+//     // deviceready Event Handler
+//     //
+//     // The scope of 'this' is the event. In order to call the 'receivedEvent'
+//     // function, we must explicitly call 'app.receivedEvent(...);'
+//     onDeviceReady: function() {
+//         app.receivedEvent('deviceready');
+//     },
+//     // Update DOM on a Received Event
+//     receivedEvent: function(id) {
+//         var parentElement = document.getElementById(id);
+//         var listeningElement = parentElement.querySelector('.listening');
+//         var receivedElement = parentElement.querySelector('.received');
+
+//         listeningElement.setAttribute('style', 'display:none;');
+//         receivedElement.setAttribute('style', 'display:block;');
+
+//         console.log('Received Event: ' + id);
+//     }
+// };
---
+++ www/js/login.js
@@ -0,0 +1,10 @@
+'use strict';
+angular.module('login', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/login', {
+		templateUrl: 'view/login.html',
+		controller: 'LoginController'
+	});
+}])
+.controller('LoginController', [function() {
+}]);
