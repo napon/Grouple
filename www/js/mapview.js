74d94a2eda81e41e2a162e1d1070ef9358ede864
Napon Taratan <napon@napontaratan.com>
2015-02-21 17:40:01 -0800

    moved index out of view

--- www/js/mapview.js
+++ www/js/mapview.js
@@ -7,24 +7,4 @@ angular.module('grouple')
         $scope.events = snapshot.val();
         console.log(snapshot.val());
       });
-
-    // $scope.map = { center: { latitude: 49.2827, longitude: -123.1 }, zoom: 12 };
-
 }]);
-
-
-
-
-// angular.module('grouple')
-// .controller('mapViewController', 
-//   function($scope) {
-//     var ref = new Firebase("https://grouple.firebaseio.com/events/");
-
-//       ref.on('value', function(snapshot) {
-//         $scope.events = snapshot.val();
-//         console.log(snapshot.val());
-//       });
-
-//     $scope.map = { center: { latitude: 49.2827, longitude: -123.1 }, zoom: 12 };
-
-// });
