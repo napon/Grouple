6a8b8eac01427393b23b2f1758709aa4aa21e640
Napon Taratan <napon@napontaratan.com>
2015-02-21 19:09:43 -0800

    fixed map problem

--- www/js/communityTweet.js
+++ www/js/communityTweet.js
@@ -1,5 +1,5 @@
 angular.module('grouple')
-.controller('CommunityTweetController', function($scope, $http) {
+.controller('CommunityTweetController', function($scope, $http, $location) {
 	'use strict';
 	function fetchPublicTweets() {
 		var endpoint = 'http://hub.urbanopus.net/wotkit/api/sensors/';
@@ -14,6 +14,14 @@ angular.module('grouple')
 			});
 	
 	}
+
+	$scope.findgroups = function() {
+        $location.path('/map');
+    };
+
+    $scope.instafeed = function() {
+        $location.path('/tweets');
+    };
 
 	function parseTwitterFeed(response) {
 		return response.map(function(obj) {
--- www/js/mapview.js
+++ www/js/mapview.js
@@ -1,10 +1,28 @@
-angular.module('grouple')
-.controller('mapViewController', ['$scope',
-  function($scope) {
-    var ref = new Firebase("https://grouple.firebaseio.com/events/");
+var globalevents;
 
+angular.module('grouple')
+.controller('mapViewController', ['$scope', '$location',
+  function($scope, $location) {
+    $scope.findgroups = function() {
+        $location.path('/map');
+    };
+
+    $scope.instafeed = function() {
+        $location.path('/tweets');
+    };
+
+    var ref = new Firebase("https://grouple.firebaseio.com/events/");
       ref.on('value', function(snapshot) {
-        $scope.events = snapshot.val();
-        console.log(snapshot.val());
+        // $scope.events = snapshot.val();
+        var events = [];
+        var returnedObj = snapshot.val();
+        Object.keys(returnedObj) && 
+        Object.keys(returnedObj).map(function(key) {
+          events.push(returnedObj[key]);
+        });
+        $scope.events = events;
+        $scope.$apply(function() {
+          $scope.events = events;
+        });
       });
 }]);
